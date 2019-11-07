4e3120eb4185eebac825f6d9e41dd8362657b1aa
Mikael Aurell <aurell.mikael@gmail.com>
2019-11-07 11:21:03 +0100

    Finished Person and added Unittest

--- Todo_Project/Model/Person.cs
+++ Todo_Project/Model/Person.cs
@@ -4,8 +4,93 @@ using System.Text;
 
 namespace Todo_Project.Model
 {
-    class Person
+    public class Person
     {
+        private string firstName;
+        private string lastName;
+        public int PersonId { get; private set; }
+        
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            
+            private set
+            {
+                bool checkLetter = true;
+                if (value.Length > 0 && value.Length < 60 && value != null)
+                {
+                    foreach (var item in value)
+                    {
+                        if (!char.IsLetter(item))
+                        {
+                            checkLetter=false; 
+                        }
+                    }
+                    if (checkLetter)
+                    {
+                        firstName = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Name can only contain letters.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Name is too long or short.");
+                }            
+            }
+        
+        }
 
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            private set
+            {
+                bool checkLetter = true;
+                if (value.Length > 0 && value.Length < 60 && value != null)
+                {
+
+                    foreach (var item in value)
+                    {
+                        if (!char.IsLetter(item))
+                        {
+                            checkLetter = false;
+                        }
+                    }
+                    if (checkLetter)
+                    {
+                        lastName = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Name can only contain letters.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Name is too long or short.");
+                }
+            }
+
+        }
+        
+        
+        public Person (string firstname, string lastname) //Constructor
+        {
+            PersonId=1;
+            //PersonId = PersonSequencer.NextPersonId;
+            FirstName = firstname;
+            LastName = lastname;
+
+        }
     }
 }
---
+++ XUnitTest_Todo_Project/UnitTestPerson.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+using Todo_Project.Model;
+
+namespace XUnitTest_Todo_Project
+{
+    public class UnitTestPerson
+    {
+        [Fact]
+        public void CreatePerson_ok()
+        {
+            //Arrange
+            
+            string firstName = "testfirstname";
+            string lastName = "testlastname";
+
+            //  Act
+            Person testPerson = new Person(firstName, lastName);
+
+            //Assert
+            Assert.Equal(firstName, testPerson.FirstName);
+            Assert.Equal(lastName, testPerson.LastName);
+
+        }
+        [Fact]
+        public void CreatePerson_not_letter()
+        {
+            //Arrange
+
+            string firstName = "testname";
+            string lastName = "7";
+            string excpected_error_msg = "Name can only contain letters.";
+            string error_msg = "";
+            Person testPerson = null;
+
+            //  Act
+            try
+            {
+            testPerson = new Person(firstName, lastName);
+            }
+
+            catch (ArgumentException exception)
+            {
+                error_msg = exception.Message;
+            }
+            //Assert
+            Assert.Equal(excpected_error_msg, error_msg);           
+        }
+
+        [Fact]
+        public void CreatePerson_not_null()
+        {
+            //Arrange
+
+            string firstName = "testname";
+            string lastName = "";
+            string excepted_error_msg = "Name is too long or short.";
+            string error_msg = "";
+            Person testPerson = null;
+
+            //  Act
+            try
+            {
+                testPerson = new Person(firstName, lastName);
+            }
+
+            catch (ArgumentException exception)
+            {
+                error_msg = exception.Message;
+            }
+            //Assert
+            Assert.Equal(excepted_error_msg, error_msg);
+        }
+    }
+}
