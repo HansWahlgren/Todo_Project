8cefe40e9a902c9edda885578ae26dcd7dbf5f6b
Philip Wahlgren <phwahlgren@gmail.com>
2019-11-07 08:53:07 +0100

    Add project files.

---
+++ Todo_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Todo_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Project done by Philip and Mikael
+        }
+    }
+}
