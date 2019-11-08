a7aa5f3af3a8581791910cf583dc934391093fb0
Mikael Aurell <aurell.mikael@gmail.com>
2019-11-08 10:46:29 +0100

    Added People without Unit Test

---
+++ XUnitTest_Todo_Project/UnitTestPeople.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Todo_Project.Data;
+
+namespace XUnitTest_Todo_Project
+{
+    public class UnitTestPeople
+    {
+        [Fact]
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
